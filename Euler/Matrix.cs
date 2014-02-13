d81600daad0c763b812c2a52ba15da67c131c166
ryanohs <rohs@phxwg.com>
2014-02-12 20:42:47 -0600

    Problem 11.

--- Euler/Matrix.cs
+++ Euler/Matrix.cs
@@ -22,5 +22,49 @@ namespace Euler
 				}
 			}
 		}
+
+		public static void HorizontalMatch(int[][] matrix, int x, int y, int length, Action<int[]> action)
+		{
+			for (int i = 0; i < y; i++)
+			{
+				for (int j = 0; j < x-length; j++)
+				{
+					action(new[] {matrix[i][j], matrix[i][j + 1], matrix[i][j + 2], matrix[i][j + 3]});
+				}
+			}
+		}
+
+		public static void VerticalMatch(int[][] matrix, int x, int y, int length, Action<int[]> action)
+		{
+			for (int i = 0; i < y - length; i++)
+			{
+				for (int j = 0; j < x; j++)
+				{
+					action(new[] { matrix[i][j], matrix[i+1][j], matrix[i+2][j], matrix[i+3][j] });
+				}
+			}
+		}
+
+		public static void LeftDiagonalMatch(int[][] matrix, int x, int y, int length, Action<int[]> action)
+		{
+			for (int i = 0; i < y - length; i++)
+			{
+				for (int j = 0; j < x - length; j++)
+				{
+					action(new[] { matrix[i][j], matrix[i + 1][j+1], matrix[i + 2][j+2], matrix[i + 3][j+3] });
+				}
+			}
+		}
+
+		public static void RightDiagonalMatch(int[][] matrix, int x, int y, int length, Action<int[]> action)
+		{
+			for (int i = length-1; i < y; i++)
+			{
+				for (int j = 0; j < x - length; j++)
+				{
+					action(new[] { matrix[i][j], matrix[i-1][j + 1], matrix[i -2][j + 2], matrix[i -3][j + 3] });
+				}
+			}
+		}
 	}
 }
